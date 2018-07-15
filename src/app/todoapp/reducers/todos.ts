fbe581ccf82847f34519110170ac46b51a658e4f
raydedon <raydedon@gmail.com>
2018-07-15 23:37:59 +0530

    js to ts

--- src/app/todoapp/reducers/todos.ts
+++ src/app/todoapp/reducers/todos.ts
@@ -1,6 +1,5 @@
 import {
 	ActionTypeKeys,
-	IEditTodo,
 	ITodo,
 	ITodoId, ITodosList,
 	TodosListActionTypes
@@ -11,6 +10,8 @@ const list : (state: Array<ITodo>, action: TodosListActionTypes) => Array<ITodo>
 	switch(type) {
 		case ActionTypeKeys.CREATE_TODO_SUCCESS:
 			return [...state, {...payload as ITodo}];
+			//todo
+/*
 		case ActionTypeKeys.TODO_MARK_COMPLETED_SUCCESS:
 			return state.map(i => {
 				return i.id === (payload as ITodo).id ? {...i, completed: !i.completed} : i;
@@ -19,6 +20,7 @@ const list : (state: Array<ITodo>, action: TodosListActionTypes) => Array<ITodo>
 			return state.map(i => {
 				return i.id === (payload as IEditTodo).id ? {...i, text: (payload as IEditTodo).text} : i;
 			});
+*/
 		case ActionTypeKeys.FETCH_TODOS_SUCCESS:
 			return (payload as ITodosList).list;
 		case ActionTypeKeys.DELETE_TODO_SUCCESS:
