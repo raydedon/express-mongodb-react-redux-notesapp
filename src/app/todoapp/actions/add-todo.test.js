31317d03a05c6625234ddf8dee1b943f54853179
raydedon <raydedon@gmail.com>
2018-07-29 23:30:20 +0530

    jest unt tests

--- src/app/todoapp/actions/add-todo.test.js
+++ src/app/todoapp/actions/add-todo.test.js
@@ -1,11 +1,23 @@
-import {CREATE_TODO_REQUEST, CREATE_TODO_SUCCESS, createTodo, createTodoSuccess} from './add-todo';
+import {
+	CREATE_TODO_REQUEST,
+	CREATE_TODO_SUCCESS,
+	createTodo,
+	createTodoSuccess,
+	createTodoRequest,
+	onAddTodoTextChange,
+	fetchTodosRequest,
+	fetchTodosSuccess,
+	FETCH_TODOS_REQUEST,
+	FETCH_TODOS_SUCCESS,
+	fetchTodos
+} from './add-todo';
 import thunk from 'redux-thunk';
 import configureMockStore from 'redux-mock-store';
 import fetchMock from 'fetch-mock';
-import {ROOT_URL} from "../../utility";
+import {ROOT_URL} from '../../utility';
 
 describe('actions', () => {
-	it('should recieve todo', () => {
+	it('should create todo', () => {
 		const text = 'Finish docs', id = '123', completed = false;
 		const expectedAction = {
 			type: 'CREATE_TODO_SUCCESS',
@@ -17,7 +29,40 @@ describe('actions', () => {
 			completed
 		})).toEqual(expectedAction);
 	});
-	
+
+	it('should create action of type CREATE_TODO_REQUEST when createTodoRequest action creater is called', () => {
+		const expectedAction = {
+			type: 'CREATE_TODO_REQUEST'
+		};
+		expect(createTodoRequest()).toEqual(expectedAction);
+	});
+
+	it('should create action of type ADD_TODO_INPUT_TEXT_CHANGE when onAddTodoTextChange action creater is called', () => {
+		const expectedAction = {
+			type: 'ADD_TODO_INPUT_TEXT_CHANGE',
+			payload: {
+				text: 'Hi'
+			}
+		};
+		expect(onAddTodoTextChange('Hi')).toEqual(expectedAction);
+	});
+
+	it('should create action of type FETCH_TODOS_REQUEST when fetchTodosRequest action creater is called', () => {
+		const expectedAction = {
+			type: 'FETCH_TODOS_REQUEST'
+		};
+		expect(fetchTodosRequest()).toEqual(expectedAction);
+	});
+
+	it('should create action of type FETCH_TODOS_SUCCESS when fetchTodosSuccess action creater is called', () => {
+		const expectedAction = {
+			type: 'FETCH_TODOS_SUCCESS',
+			payload: {
+				list: []
+			}
+		};
+		expect(fetchTodosSuccess([])).toEqual(expectedAction);
+	});
 });
 
 
@@ -66,4 +111,39 @@ describe('async actions', () => {
 		});
 	});
 
+	it('creates FETCH_TODOS_REQUEST, FETCH_TODOS_SUCCESS when fetchTodos action creater is called', () => {
+		fetchMock
+			.getOnce(`${ROOT_URL}/todos`, {
+				body: [{
+					_id: '5b58addaaa0f9913ec128012',
+					text: 'housing',
+					completed: true,
+					createdAt: '2018-06-30T12:19:41.413Z',
+					updatedAt: '2018-07-01T06:12:55.303Z',
+					__v: 0,
+					id: '5b58addaaa0f9913ec128012'
+				}],
+				headers: {'content-type': 'application/json'}
+			});
+
+
+		const expectedActions = [
+			{
+				type: FETCH_TODOS_REQUEST
+			},
+			{
+				type: FETCH_TODOS_SUCCESS,
+				payload: {
+					id: '5b58addaaa0f9913ec128012',
+					text: 'housing',
+					completed: true
+				}
+			}
+		];
+
+		store.dispatch(fetchTodos()).then(() => {
+			// return of async actions
+			expect(store.getActions()).toEqual(expectedActions);
+		});
+	});
 });
